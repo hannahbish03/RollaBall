be4045c006af5915528a5ec7f4a3abb37faf5857
hannahbish <510894@chrome.escg.ac.uk>
2020-09-24 15:37:32 +0100

    ball game

---
+++ Assets/script/NewBehaviourScript.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Rigidbody rb;
+    private float movementX;
+    private float movementY;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Onmove(InputValue movementvalue)
+    {
+
+
+        Vector2 movementvector = movementvalue.Get<Vector2>();
+
+
+        movementX = movementVector.X;
+        movementY = movementVector.Y;
+    }
+
+
+
+    void FixedUpdate()
+    {
+
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+
+        rb.AddForce(movementVector);
+
+    }
+
+
+    {
+
+      Vector3 movement 
+
+    }
+}
+
+
